fd3147bb994b946f1d717fcebd6cdd29ef972bf8
wugren <wugren@gmail.com>
2024-09-19 19:26:33 +0800

    Support dynamic addition of schema

--- src/openapi.rs
+++ src/openapi.rs
@@ -18,12 +18,32 @@ macro_rules! add_openapi_item {
     };
 }
 
+#[macro_export]
+macro_rules! add_openapi_schema {
+    ($api_doc: expr, $name: ident) => {
+        sfo_http::openapi::paste! {
+            sfo_http::openapi::OpenApiServer::add_schema_item::<$name>($api_doc);
+        }
+    };
+}
+
 #[cfg(feature = "openapi")]
 pub trait OpenApiServer {
     fn set_api_doc(&mut self, api_doc: openapi::OpenApi);
     fn get_api_doc(&mut self) -> &mut openapi::OpenApi;
     fn add_api_item<P: Path>(&mut self) {
         self.get_api_doc().paths.paths.insert(P::path(), P::path_item(Some("")));
+    }
+
+    fn add_schema_item<S: for<'a> ToSchema<'a>>(&mut self) {
+        if self.get_api_doc().components.is_none() {
+            self.get_api_doc().components = Some(openapi::Components::default());
+        }
+        let (name, obj) = S::schema();
+        if self.get_api_doc().components.as_mut().unwrap().schemas.contains_key(&name.to_string()) {
+            return;
+        }
+        self.get_api_doc().components.as_mut().unwrap().schemas.insert(name.to_string(), obj);
     }
 }
 
