9d91a0c0897d5f3df019d9c1bb5b550e8b02c0ae
wugren <wugren@gmail.com>
2024-09-24 16:53:13 +0800

    add jwt api

--- src/token_helper.rs
+++ src/token_helper.rs
@@ -5,22 +5,22 @@ pub use jsonwebtoken::*;
 pub type TokenResult<T> = errors::Result<T>;
 
 #[derive(Serialize, Deserialize)]
-struct Payload<T> {
+pub struct Payload<T> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    iss: Option<String>, //签发人
+    pub iss: Option<String>, //签发人
     #[serde(skip_serializing_if = "Option::is_none")]
-    exp: Option<u64>, //过期时间
+    pub exp: Option<u64>, //过期时间
     #[serde(skip_serializing_if = "Option::is_none")]
-    sub: Option<String>, //主题
+    pub sub: Option<String>, //主题
     #[serde(skip_serializing_if = "Option::is_none")]
-    aud: Option<String>, //受众
+    pub aud: Option<String>, //受众
     #[serde(skip_serializing_if = "Option::is_none")]
-    nbf: Option<u64>, //生效时间
+    pub nbf: Option<u64>, //生效时间
     #[serde(skip_serializing_if = "Option::is_none")]
-    iat: Option<u64>, //签发时间
+    pub iat: Option<u64>, //签发时间
     #[serde(skip_serializing_if = "Option::is_none")]
-    jti: Option<u64>, //编号
-    data: T,
+    pub jti: Option<u64>, //编号
+    pub data: T,
 }
 
 pub struct JsonWebToken;
@@ -50,4 +50,11 @@ impl JsonWebToken {
         Ok(token_data.claims.data)
     }
 
+    pub fn decode_payload<T: for<'a> Deserialize<'a>>(token: &str, key: &DecodingKey) -> TokenResult<Payload<T>> {
+        let header = jsonwebtoken::decode_header(token)?;
+        let mut val = Validation::new(header.alg);
+        val.validate_exp = true;
+        let token_data: TokenData<Payload<T>> = jsonwebtoken::decode(token, key, &val)?;
+        Ok(token_data.claims)
+    }
 }
